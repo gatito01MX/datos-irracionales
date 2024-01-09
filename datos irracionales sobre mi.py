ac4589684cb03ea6c9157448fde59251fc3b7c02
gatito01MX <156039815+gatito01MX@users.noreply.github.com>
2024-01-08 20:30:14 -0600

    Create datos irracionales sobre mi.py
    datos sobre mi :V

---
+++ datos irracionales sobre mi.py
@@ -0,0 +1,13 @@
+# no se como sorprenderlos .... tengo el platino de left 4 dead 2 
+import random
+
+my_words = ["hola", "me da hipe GTAIV", "Kodland"] 
+print(my_words[1])
+
+
+
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)", "<3", "-_/", ":V", " XD", "UWU", ":P"]
+print(random.choice(emojis))
+
+datos = ["tengo gustos raros", "soy asocial", "streamer", "furro", "webiwabo", "tengo 15 años", "tengo platinados 7 juegos"]
+print(random.choice(datos))
